cdc4c91b2f3183e889014c7aa825731f9579bbdd
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-08-12 17:48:52 +0300

    Add basic Homepage component

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Homepage = () => {
+    return (
+        <div>
+            <h2>HomePage</h2>
+        </div>
+    )
+}
+
+
+export default HomePage
