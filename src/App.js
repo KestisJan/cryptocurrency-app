d13d348a26369f93e4cfa6914c563c3694ecd021
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-08-12 18:33:23 +0300

    Implement new NavBar component and update Homepage
    - Added new NavBar component (NavBar.jsx) for navigation.
    - Updated Homepage.jsx to include the new NavBar.
    - Created index.js in components to streamline imports.
    - Updated App.js and index.js to integrate NavBar and other components.
    - Added cryptologo.png image for use in the NavBar.
    - Updated package.json and package-lock.json for new dependencies.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd'
+
+import { NavBar, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'
+
 
 const App = () => {
     return (
-        <div>
-            <h1>Test</h1>
-        </div>
+       <div className='app'>
+            <div className='navbar'>
+                <NavBar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route path='/' element={<Homepage />}/>
+                            <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+                            <Route path='/news' element={<News/>} />
+                        </Routes>
+                    </div>
+                </Layout>
+            </div>
+            <div className='footer'>
+                <Typography.Title className='footer' level={5} style={{ color: 'white', textAlign: 'center' }}>
+                    CryptoApp <br/>
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
+            </div>
+       </div>
     )
 }
 
