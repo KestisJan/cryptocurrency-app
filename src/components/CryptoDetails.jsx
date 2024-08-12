b31670c7c2c24adc9b4731540e73619f6ec72dfc
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-08-12 17:56:40 +0300

    Add CryptoDetails component

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+
+const CryptoDetails = () => {
+    return (
+        <div>
+            <h2>Crypto Details</h2>
+        </div>
+    )
+}
+
+export default CryptoDetails
