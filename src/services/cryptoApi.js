7adc5948875910b21c26f74dbc115c30244c71ea
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-08-12 19:01:46 +0300

    Add cryptoApi service
    - Created src/services/cryptoApi.js to handle API interactions for cryptocurrency data.

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl,
+        prepareHeaders: (headers) => {
+            headers.set('x-rapidapi-host', 'coinranking1.p.rapidapi.com');
+            headers.set('x-rapidapi-key', 'fb3d5c5d05mshb0f32340e03b7cdp157fe2jsn4d24b4b077cf');
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => `/coins?limit=${count}`,
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
