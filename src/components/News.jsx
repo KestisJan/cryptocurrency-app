647a6bfc19b9f9dd4f7f3eb41d89845c63bc7f05
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-08-12 18:16:03 +0300

    Add News component

---
+++ src/components/News.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+
+const News = () => {
+    return (
+        <div>
+            <h2>News</h2>
+        </div>
+    )
+}
+
+export default News
